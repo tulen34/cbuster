1edca89a22dc1feda166cc8ee16d4f47b9df814d
Глушонков Илья <pulsar04040@gmail.com>
2021-05-19 16:25:20 +0300

    Add `struct buster_dir`

--- include/dir.h
+++ include/dir.h
@@ -1,6 +1,19 @@
 #ifndef _DIR_H
 #define _DIR_H
 
+#include <stdio.h>
 
+struct buster_dir {
+    FILE *wordlist_stream;
+    unsigned int nthreads;
+
+    char *host;
+    char *user_agent;
+    char *http_username, *http_password;
+
+    char *proxy, *proxy_username, *proxy_password;
+};
+
+struct buster_dir *buster_dir_init(char *host);
 
 #endif
--- src/dir.c
+++ src/dir.c
@@ -1,1 +1,19 @@
 #include <dir.h>
+#include <stdio.h>
+
+struct buster_dir *buster_dir_init(char *host) {
+    struct buster_dir *ret = (struct buster_dir *)malloc(
+            sizeof(struct buster_dir));
+    ret->wordlist_stream = stdin;
+    ret->nthreads = 10U
+
+    ret->host = host;
+    ret->user_agent = "cbuster";
+    ret->http_username = NULL;
+    ret->http_password = NULL;
+
+    ret->proxy = NULL;
+    ret->proxy_username = NULL;
+    ret->proxy_password = NULL;
+    return ret;
+}
