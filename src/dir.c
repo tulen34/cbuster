2f54cb4f3ff381bdbc5df023a4e632c87977f84b
Глушонков Илья <pulsar04040@gmail.com>
2021-05-20 14:24:50 +0300

    Add function `buster_dir_set_rand_user_agent()`

--- src/dir.c
+++ src/dir.c
@@ -1,20 +1,47 @@
 #include <dir.h>
+#include <utils.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
 
-struct buster_dir *buster_dir_init(char *host) {
-    struct buster_dir *ret = (struct buster_dir *)malloc(
+extern int errno;
+
+struct buster_dir *buster_dir_init(const char *host, FILE *wordlist) {
+    struct buster_dir *dir = (struct buster_dir *)malloc(
             sizeof(struct buster_dir));
-    ret->wordlist_stream = stdin;
-    ret->nthreads = 10U;
 
-    ret->host = host;
-    ret->user_agent = "cbuster";
-    ret->http_username = NULL;
-    ret->http_password = NULL;
+    if (host == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
+    strcpy(dir->host, host);
 
-    ret->proxy = NULL;
-    ret->proxy_username = NULL;
-    ret->proxy_password = NULL;
-    return ret;
+    dir->wordlist = wordlist == NULL ? stdin : wordlist;
+    sprintf(dir->user_agent, "cbuster/%s", BUSTER_VERSION);
+    dir->nthreads = 8U;
+    return dir;
+}
+
+int buster_dir_set_rand_user_agent(struct buster_dir *dir, const char *path) {
+    FILE *fp = fopen(path, "r");
+    if (fp == NULL) {
+        errno = ENOENT;
+        return errno;
+    }
+
+    unsigned int nlines = 0;
+    int c;
+    while ((c = fgetc(fp)) != EOF)
+        if (c == '\n')
+            ++nlines;
+    rewind(fp);
+
+    srand(time(NULL));
+    int nline = rand() % nlines + 1;
+    for (int i = 0; i < nline; ++i)
+        fgets(dir->user_agent, STRSIZE, fp);
+    return EXIT_SUCCESS;
 }
