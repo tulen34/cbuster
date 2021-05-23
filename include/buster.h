b15c09e120ceeeeae438e95d72fcecb4ec991c28
Глушонков Илья <pulsar04040@gmail.com>
2021-05-23 10:06:04 +0300

    Rename utils.h to buster.h

---
+++ include/buster.h
@@ -0,0 +1,6 @@
+#ifndef _BUSTER_H
+#define _BUSTER_H
+
+#define BUSTER_VERSION "0.1"
+
+#endif // _BUSTER_H
--- include/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef _UTILS_H
-#define _UTILS_H
-
-#define UTILS_VERSION "0.1"
-#define UTILS_STRSIZE 1024
-
-#endif // _UTILS_H
