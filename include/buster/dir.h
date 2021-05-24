4d3f5a03dcdafd5b10cb2cfbb2566814d1f4345a
Глушонков Илья <pulsar04040@gmail.com>
2021-05-24 16:55:18 +0300

    Simplify dir.h

--- include/buster/dir.h
+++ include/buster/dir.h
@@ -1,26 +1,14 @@
 #ifndef _DIR_H
 #define _DIR_H
 
-#include <curl/curl.h>
 #include <stdio.h>
 
-struct buster_dir_optionals {
-    bool use_rand_useragent;
-
-    FILE *output;
-};
-
-struct buster_dir {
+// TODO: Add optional fields
+struct busterdir {
     char *host;
     FILE *wordlist;
-
-    struct buster_dir_optionals optionals;
-
-    pthread_mutex_t *mutex;
 };
 
-struct buster_dir buster_dir_init(char *host, FILE *wordlist);
-
-void buster_dir_start(struct buster_dir dir);
+int busterdir_run(struct busterdir dir);
 
 #endif // _DIR_H
