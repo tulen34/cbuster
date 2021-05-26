b0afcf10d4803c6338e9d25dc36de54ed556448b
Глушонков Илья <pulsar04040@gmail.com>
2021-05-26 22:18:18 +0300

    Update function `buster_wordlist_gets`

--- src/buster.c
+++ src/buster.c
@@ -1,12 +1,14 @@
 #include <buster.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 char *buster_wordlist_gets(char *restrict s, size_t n, FILE *restrict stream) {
     fgets(s, n, stream);
 
-    char *comment = strchr(s, '#'); 
-    if (comment != NULL)
-        *comment = '\0';
+    for (int i = 0; s[i] != '\0'; ++i)
+        // Ignore comments and all after space
+        if (isspace(s[i]) || s[i] == '#')
+            s[i] = '\0';
     return s;
 }
