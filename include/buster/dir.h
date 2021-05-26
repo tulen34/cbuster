b9bf03285d7d5761c6129cf9972e0ce3442207d1
Глушонков Илья <pulsar04040@gmail.com>
2021-05-26 22:17:23 +0300

    Add functions `busterdir_wordlist_geturl` and `busterdir_getranduseragent`

--- include/buster/dir.h
+++ include/buster/dir.h
@@ -3,11 +3,23 @@
 
 #include <stdio.h>
 
+#ifndef
+# define USERAGENTS_PATH "etc/dir/user-agents.txt"
+#endif
+
 // TODO: Add optional fields
 struct busterdir {
     char *host;
     FILE *wordlist;
+
+    size_t nhandles;
+    bool use_randuseragents;
 };
+
+char *busterdir_wordlist_geturl(char *restrict s, size_t n,
+                                const char *host, FILE *restrict wordlist);
+
+char *busterdir_getranduseragent(char *restrict s, size_t n);
 
 int busterdir_run(struct busterdir dir);
 
