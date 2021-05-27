ae81900cc6559aeb9c09a8e3be156681b6e4a8a5
Глушонков Илья <pulsar04040@gmail.com>
2021-05-27 09:26:45 +0300

    Add func `busterdir_run`

--- src/dir.c
+++ src/dir.c
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+#include <errno.h>
 #include <curl/curl.h>
 
 #define STRSIZE 1064
@@ -33,7 +35,7 @@ char *busterdir_getranduseragent(char *restrict s, size_t n) {
 
     int rand_nline = rand() % nlines + 1;
     for (int i = 0; i < rand_nline; ++i)
-        fgets(s, STRSIZE, fp);
+        fgets(s, n, fp);
 ret:
     return s;
 }
@@ -45,7 +47,7 @@ int busterdir_curl_multi_add_handle(struct busterdir dir,
         return EIO;
 
     // Setting output to /dev/null
-    curl_easy_setopt(handles[i], CURLOPT_WRITEDATA, devnull);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, devnull);
 
     // Getting url using wordlist
     char url[STRSIZE];
@@ -63,5 +65,56 @@ int busterdir_curl_multi_add_handle(struct busterdir dir,
     curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent);
 
     curl_multi_add_handle(curlm, curl);
+    return EXIT_SUCCESS;
+}
+
+void check_curlmsg(CURLM *curlm) {
+    struct CURLMsg *m;
+    do {
+        int msgs_in_queue;
+        m = curl_multi_info_read(curlm, &msgs_in_queue);
+        if (m == NULL || m->msg != CURLMSG_DONE)
+            continue;
+
+        CURL *curl = m->easy_handle;
+
+        long code;
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &code);
+
+        char *method, *url;
+        curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_METHOD, &method);
+        curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
+
+        char output[STRSIZE];
+        sprintf(output, "%-40s - %-7s - %ld", url, method, code);
+        puts(output);
+
+        curl_multi_remove_handle(curlm, curl);
+        curl_easy_cleanup(curl);
+    } while (m != NULL);
+}
+
+int busterdir_run(struct busterdir dir) {
+    CURLM *curlm = curl_multi_init();
+    if (curlm == NULL)
+        return EIO;
+
+    FILE *devnull = fopen("/dev/null", "w+");
+    if (devnull == NULL)
+        return ENOENT;
+
+    while (!feof(dir.wordlist)) {
+        for (size_t i = 0; i < dir.nhandles; ++i) {
+            int status = busterdir_curl_multi_add_handle(dir, curlm, devnull);
+            if (status != EXIT_SUCCESS)
+                return status;
+        }
+
+        int nrunning;
+        do {
+            curl_multi_perform(curlm, &nrunning);
+            check_curlmsg(curlm);
+        } while (nrunning > 0);
+    }
     return EXIT_SUCCESS;
 }
