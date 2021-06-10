4d11fab8221c7f7ac09ae193167b9d5609c032c6
Глушонков Илья <pulsar04040@gmail.com>
2021-06-10 16:08:03 +0300

    Add implementation for `wordlist_init`

--- src/buster.c
+++ src/buster.c
@@ -1,14 +1,49 @@
 #include <buster.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-char *buster_wordlist_gets(char *restrict s, size_t n, FILE *restrict stream) {
-    fgets(s, n, stream);
+struct wordlist *wordlist_init(FILE *stream) {
+    struct wordlist *wl = malloc(sizeof(struct wordlist));
+    if (wl == NULL)
+        return NULL;
 
-    for (int i = 0; s[i] != '\0'; ++i)
-        // Ignore comments and all after space
-        if (isspace(s[i]) || s[i] == '#')
-            s[i] = '\0';
-    return s;
+    fseek(stream, 0L, SEEK_END);
+    long nbytes = ftell(stream);
+    rewind(stream);
+
+    char *text = calloc((size_t)nbytes, sizeof(char));
+    if (text == NULL) {
+        free(wl);
+        return NULL;
+    }
+    fread(text, sizeof(char), (size_t)nbytes, stream);
+
+    int nlines = 0;
+    for (int i = 0; i < nbytes; ++i)
+        if (text[i] == '\n')
+            ++nlines;
+
+    wl->buf = calloc((size_t)nlines, sizeof(char *));
+    if (wl->buf == NULL) {
+        free(wl);
+        free(text);
+        return NULL;
+    }
+
+    int nchars = 0;
+    int top = 0;
+    for (int i = 0; i < nbytes; ++i) {
+        if (text[i] != '\n' && text[i] != '#') {
+            ++nchars;
+            continue;
+        }
+        char *start = text + i - nchars;
+        wl->buf[top++] = strndup(start, (size_t)nchars - 1);
+        nchars = 0;
+    }
+
+    free(text);
+    return wl;
 }
