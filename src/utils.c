44e699daa42d8e8c0c131f7c34458bc15ace00a9
Глушонков Илья <pulsar04040@gmail.com>
2021-06-30 17:59:30 +0300

    Update utils

--- src/utils.c
+++ src/utils.c
@@ -1,24 +1,51 @@
 #include <utils.h>
-
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #define STRSIZE 4096
+#define FORMAT "%[^#\n]s\n"
 
-char **wordlist_read(FILE *stream) {
-    int ch, nlines, stat, i;
-    char **wl, s[STRSIZE];
+struct bwordlist *
+bwordlist_read(FILE *stream) {
+    int ch, nlines, stat;
+    char s[STRSIZE];
+    struct bwordlist *wl;
+
+    wl = malloc(sizeof(struct bwordlist));
+    if (wl == NULL) 
+        goto enomem;
 
     nlines = 0;
     while ((ch = fgetc(stream)) != EOF)
-        if (ch == '\n') ++nlines;
-    rewind(stream);
+        if (ch == '\n') 
+            ++nlines;
+    wl->buf = calloc((size_t)nlines, sizeof(char *));
+    if (wl->buf == NULL) 
+        goto enomem;
 
-    wl = calloc((size_t)nlines, sizeof(char *));
-    if (wl == NULL) return NULL;
+    while ((stat = fscanf(stream, FORMAT, s)) != EOF) {
+        if (stat != 1)
+            continue;
+        if ((wl->buf[wl->len++] = strdup(s)) == NULL)
+            goto enomem;
+    }
 
-    i = 0;
-    while ((stat = fscanf(stream, "%[^#\n]s\n", s)) != EOF)
-        if (stat == 1) wl[i++] = strdup(s);
     return wl;
+enomem:
+    if (wl != NULL) 
+        bwordlist_free(wl);
+    errno = ENOMEM;
+    return NULL;
+}
+
+void 
+bwordlist_free(struct bwordlist *wl) {
+    int i;
+
+    for (i = 0; i < (long)wl->len; ++i) 
+        if (wl->buf[i] != NULL)
+            free(wl->buf[i]);
+    free(wl->buf);
+    free(wl);
 }
--- src/utils.h
+++ src/utils.h
@@ -2,9 +2,26 @@
 #define _UTILS_H
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
 
-#define VERSION "0.1"
+#define BUSTER_VERSION "0.1"
 
-char **wordlist_read(FILE *stream);
+struct bwordlist {
+    char **buf;
+    size_t len;
+};
+
+struct bwordlist *
+bwordlist_read(FILE *stream);
+
+void 
+bwordlist_free(struct bwordlist *wl);
+
+struct boptions {
+    struct bwordlist *wordlist;
+    time_t delay;
+    size_t routines;
+};
 
 #endif // _UTILS_H
