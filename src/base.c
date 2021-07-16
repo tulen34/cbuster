6fd2d9a8c8a742eea5e4d3cc373a40e68aa5c0e5
Глушонков Илья <pulsar04040@gmail.com>
2021-07-16 16:58:55 +0300

    Using `fgets` instead of `fscanf`

--- src/base.c
+++ src/base.c
@@ -2,39 +2,40 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <ctype.h>
 #include <errno.h>
 
 #define STRSIZE 4096
 
 int
 bwordlist_read(FILE *fp, struct bwordlist *wlp) {
-    int ch, nlines, stat;
+    int ch, nlines, i;
     char s[STRSIZE];
 
     nlines = 0;
     while ((ch = fgetc(fp)) != EOF)
-        if (ch == '\n') 
+        if (ch == '\n')
             ++nlines;
     rewind(fp);
     wlp->buf = calloc((size_t)nlines, sizeof(char *));
-    if (wlp->buf == NULL) 
+    if (wlp->buf == NULL)
         goto enomem;
 
-    while ((stat = fscanf(fp, "%[^#\n]s\n", s)) != EOF) {
-        if (stat != 1)
-            continue;
+    wlp->len = 0;
+    while (fgets(s, STRSIZE, fp) != NULL) {
+        for (i = 0; s[i] != '\0'; ++i)
+            if (s[i] == '#' || s[i] == '\n')
+                s[i] = '\0';
+
         if ((wlp->buf[wlp->len++] = strdup(s)) == NULL) {
             bwordlist_cleanup(*wlp);
             goto enomem;
         }
     }
-
     return EXIT_SUCCESS;
 enomem:
-    wlp = NULL;
     return (errno = ENOMEM);
 }
-
 
 void 
 bwordlist_cleanup(struct bwordlist wl) {
@@ -44,7 +45,6 @@ bwordlist_cleanup(struct bwordlist wl) {
         free(wl.buf[i]);
     free(wl.buf);
 }
-
 
 int
 blogger_logf(struct blogger lg, enum blogger_level level, 
